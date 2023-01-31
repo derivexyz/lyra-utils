10ff771bb78420024fd2aee7a6a8a95fb89a094b
Joshua Kim <46257136+joshpwrk@users.noreply.github.com>
2023-01-31 13:10:08 -0800

    feat: ordered and unordered memory libraries (#2)
    * feat: ordered and unordered memory libraries
    
    * remove: binarySearch since can reuse openzeppelin
    
    * chore: fmt
    
    * feat: add quick sort library
    
    * feat: docstring and fix initIndices()
    
    * testing: add testing for sorting
    
    * test: duplicate cases
    
    * gas: test worst case of 30 elements
    
    * chore: fmt

---
+++ src/arrays/QuickSort.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/**
+ * @title QuickSort
+ * @author Lyra
+ * @notice using quick-sort to sort memory arrays.
+ */
+
+library QuickSort {
+  /**
+   * @dev Sort both array and return sorted array of indexes
+   *      The data is not sorted in-memory due to recursion - a new array is returned.
+   *      E.g. [100, 10, 500] -> sorted indices: [1, 0, 2]
+   * @param data array of values to sort
+   * @return sortedData new array with sorted data
+   * @return sortedIndices array of sorted indices
+   */
+  function sort(uint[] memory data) public view returns (uint[] memory sortedData, uint[] memory sortedIndices) {
+    sortedIndices = initIndices(data.length);
+    sort(data, sortedIndices, int(0), int(data.length - 1));
+    return (data, sortedIndices);
+  }
+
+  /**
+   * @dev Use quicksort to sort array of values and indices
+   *      Inspired by: https://gist.github.com/subhodi/b3b86cc13ad2636420963e692a4d896f
+   * @param arr array of values to sort
+   * @param indices array of indices
+   * @param left left bound
+   * @param right right bound
+   */
+  function sort(uint[] memory arr, uint[] memory indices, int left, int right) public view {
+    int i = left;
+    int j = right;
+    if (i == j) return;
+    uint pivot = arr[uint(left + (right - left) / 2)];
+    while (i <= j) {
+      while (arr[uint(i)] < pivot) i++;
+      while (pivot < arr[uint(j)]) j--;
+      if (i <= j) {
+        (arr[uint(i)], arr[uint(j)]) = (arr[uint(j)], arr[uint(i)]);
+        (indices[uint(i)], indices[uint(j)]) = (indices[uint(j)], indices[uint(i)]);
+        i++;
+        j--;
+      }
+    }
+    if (left < j) {
+      sort(arr, indices, left, j);
+    }
+    if (i < right) {
+      sort(arr, indices, i, right);
+    }
+  }
+
+  /**
+   * @dev Creates an array of ordered indices, used when beginnin quickSort
+   *      E.g. with length 3 will generate array: [0, 1, 2]
+   * @param length number of values to sort
+   * @return initialIndices array of ordered indices
+   */
+  function initIndices(uint length) public pure returns (uint[] memory initialIndices) {
+    initialIndices = new uint[](length);
+    for (uint i; i < length; i++) {
+      initialIndices[i] = i;
+    }
+  }
+}
---
+++ test/arrays/QuickSort.t.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "src/arrays/QuickSort.sol";
+
+/**
+ * @dev for current `forge coverage` to work, i needs to call an external contract then invoke internal library
+ */
+contract QuickSortTester {
+  function sort(uint[] memory data) external view returns (uint[] memory sortedData, uint[] memory sortedIndices) {
+    return QuickSort.sort(data);
+  }
+}
+
+contract QuickSortTest is Test {
+  QuickSortTester tester;
+
+  function setUp() public {
+    tester = new QuickSortTester();
+  }
+
+  function testSortEven() public {
+    uint[] memory data = new uint[](6);
+    data[0] = 10;
+    data[1] = 5;
+    data[2] = 3;
+    data[3] = 100;
+    data[4] = 7;
+    data[5] = 1;
+
+    uint[] memory sortedIndices;
+    (data, sortedIndices) = tester.sort(data);
+
+    // confirm indices
+    assertEq(sortedIndices[0], 5);
+    assertEq(sortedIndices[1], 2);
+    assertEq(sortedIndices[2], 1);
+    assertEq(sortedIndices[3], 4);
+    assertEq(sortedIndices[4], 0);
+    assertEq(sortedIndices[5], 3);
+
+    // confirm data
+    assertEq(data[0], 1);
+    assertEq(data[1], 3);
+    assertEq(data[2], 5);
+    assertEq(data[3], 7);
+    assertEq(data[4], 10);
+    assertEq(data[5], 100);
+  }
+
+  function testSortOdd() public {
+    uint[] memory data = new uint[](5);
+    data[0] = 567;
+    data[1] = 243;
+    data[2] = 1;
+    data[3] = 0;
+    data[4] = 1456;
+
+    uint[] memory sortedIndices;
+    (data, sortedIndices) = tester.sort(data);
+
+    // confirm indices
+    assertEq(sortedIndices[0], 3);
+    assertEq(sortedIndices[1], 2);
+    assertEq(sortedIndices[2], 1);
+    assertEq(sortedIndices[3], 0);
+    assertEq(sortedIndices[4], 4);
+
+    // confirm data
+    assertEq(data[0], 0);
+    assertEq(data[1], 1);
+    assertEq(data[2], 243);
+    assertEq(data[3], 567);
+    assertEq(data[4], 1456);
+  }
+
+  function testSortWithDuplicates() public {
+    uint[] memory data = new uint[](4);
+    data[0] = 243;
+    data[1] = 243;
+    data[2] = 1;
+    data[3] = 243;
+
+    uint[] memory sortedIndices;
+    (data, sortedIndices) = tester.sort(data);
+
+    // confirm indices - duplicates will not be stable
+    assertEq(sortedIndices[0], 2);
+    assertEq(sortedIndices[1], 3);
+    assertEq(sortedIndices[2], 0);
+    assertEq(sortedIndices[3], 1);
+
+    // confirm data
+    assertEq(data[0], 1);
+    assertEq(data[1], 243);
+    assertEq(data[2], 243);
+    assertEq(data[3], 243);
+  }
+
+  // useful for gas estimates for board sorting
+  function testWrostCaseGasArrayOf30() public view {
+    // using sorted array as this is worst time complexity
+    uint[] memory data = new uint[](30);
+    for (uint i; i < data.length; i++) {
+      data[i] = i;
+    }
+
+    (uint[] memory sortedData, uint[] memory sortedIndices) = tester.sort(data);
+  }
+}
